2aa4fdbf8a0107fa1783f591100e0aa7309476b8
KN <ckn.kaien@gmail.com>
2025-02-08 21:55:28 +0800

    Add Pinia persistence plugin

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import { createPinia } from "pinia";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+
+const pinia = createPinia();
 
 const app = createApp(App);
-app.use(createPinia());
+
+app.use(pinia);
 app.use(router);
+pinia.use(piniaPluginPersistedstate); //  啟用 persist 插件
 app.mount("#app");
