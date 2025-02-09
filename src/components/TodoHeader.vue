be914a2995328a4d63c7d640819a22968ee55f59
KN <ckn.kaien@gmail.com>
2025-02-09 12:51:33 +0800

    refactor: migrate TodoHeader styles to Tailwind

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -31,22 +31,19 @@ const logout = async () => {
   alert("您已成功登出");
   router.push("/");
 };
-
-// onMounted(() => {
-//   userStore.fetchUser();
-// });
-
-// const test = () => {
-//   console.log("傳遞的 username:", userStore.username);
-//   console.log("目前登入使用者的 displayName:", auth.currentUser?.displayName);
-// };
 </script>
 
 <template>
   <header class="header">
-    <section class="logo">
-      <h2 class="logo__title text-white font-bold">TODO</h2>
-      <button class="btn btn--theme" @click="toggleTheme">
+    <section
+      class="flex justify-between items-center mt-12 xs:w-full xs:min-w-[327px] xs:mt-10"
+    >
+      <h2
+        class="text-white font-bold text-[3rem] leading-[4rem] tracking-[15px] xs:text-[2rem]"
+      >
+        TODO
+      </h2>
+      <button class="btn bg-transparent border-none" @click="toggleTheme">
         <img
           v-if="props.isDarkMode"
           src="../../public/images/icon-sun.svg"
@@ -55,17 +52,21 @@ const logout = async () => {
         <img v-else src="../../public/images/icon-moon.svg" alt="sunIcon" />
       </button>
     </section>
-    <div class="user__bar text-softWhite">
-      <div class="logOut__btn pb-2" @click="logout()">Log out</div>
+    <div class="flex flex-col text-softWhite text-[1.2rem]">
+      <div class="w-[30%] pb-2" @click="logout()">Log out</div>
       <div class="user__name">{{ userStore.username }}'s TodoList</div>
     </div>
-    <div class="todo__header">
-      <div class="todo__circle"></div>
+    <div
+      class="w-full bg-white p-4 xs:p-4 xs:px-6 rounded-md flex items-center mt-12 xs:mt-4 mb-10 border border-[#e3e4f1] dark:bg-[#25273d] dark:border-[#393a4b]"
+    >
+      <div
+        class="w-6 h-6 border border-[#e3e4f1] rounded-full bg-transparent dark:border-[#393a4b]"
+      ></div>
 
       <input
         v-model="inputValue"
         type="text"
-        class="todoInput"
+        class="focus:outline-none todoInput text-base placeholder:text-[#c4c4c5] border-none font-inherit leading-8 ml-6 bg-transparent dark:placeholder:text-base dark:placeholder:text-[#4d5067]"
         placeholder="Create a new todo..."
         @keyup.enter="submitTodo"
       />
@@ -75,9 +76,9 @@ const logout = async () => {
 
 <style scoped>
 /*------Todo Header-----*/
-.todo__header {
+/* .todo__header {
   width: 100%;
-  background-color: white;
+  background-color: white; 
   padding: 1rem 1.5rem;
   border-radius: 5px;
   display: flex;
@@ -85,37 +86,32 @@ const logout = async () => {
   margin-top: 3rem;
   margin-bottom: 2.4rem;
   border: 1px solid #e3e4f1;
-}
+}*/
 
-.todo__header label {
-  position: absolute;
-  left: -25vw;
-}
-
-[data-theme="darkTheme"] .todo__header {
+/* [data-theme="darkTheme"] .todo__header {
   background-color: #25273d;
   border: 1px solid #393a4b;
-}
+} */
 
-.logo {
+/* .logo {
   display: flex;
   justify-content: space-between;
   align-items: center;
   margin: 3rem 0 0 0;
-}
+} */
 
-.logo__title {
+/* .logo__title {
   font-size: 3rem;
   line-height: 4rem;
   letter-spacing: 15px;
-}
+} */
 
-.btn--theme {
+/* .btn--theme {
   background-color: transparent;
   border: none;
-}
+} */
 
-.todoInput,
+/* .todoInput,
 todoInput::-webkit-input-placeholder {
   font-size: 1rem;
   color: #393a4b;
@@ -125,17 +121,13 @@ todoInput::-webkit-input-placeholder {
 
   margin-left: 2rem;
   background: transparent;
-}
+} */
 
-.todoInput:focus {
+/* .todoInput:focus {
   outline: none;
-}
+} */
 
-[data-theme="darkTheme"] .todo__input {
-  color: inherit;
-}
-
-.todo__circle {
+/* .todo__circle {
   width: 24px;
   height: 24px;
   border: 1px solid #e3e4f1;
@@ -145,33 +137,33 @@ todoInput::-webkit-input-placeholder {
 
 [data-theme="darkTheme"] .todo__circle {
   border: 1px solid #393a4b;
-}
+} */
 
-.user__bar {
+/* .user__bar {
   display: flex;
   flex-direction: column;
   font-size: 1.2rem;
-}
+} */
 
-.logOut__btn {
+/* .logOut__btn {
   width: 15%;
-}
+} */
 
-@media (max-width: 738px) {
-  .todo {
+/* @media (max-width: 738px) { */
+/* .todo {
     width: 100%;
     min-width: 327px;
-  }
+  } */
 
-  .logo {
+/* .logo {
     margin: 3.5rem 0 0 0;
-  }
+  } */
 
-  .logo__title {
+/* .logo__title {
     font-size: 2rem;
-  }
+  } */
 
-  .todo__header {
+/* .todo__header {
     padding: 1rem 1.5rem;
     border-radius: 5px;
     display: flex;
@@ -179,6 +171,6 @@ todoInput::-webkit-input-placeholder {
     margin-top: 1rem;
     margin-bottom: 2.4rem;
     border: 1px solid #e3e4f1;
-  }
-}
+  } */
+/* } */
 </style>
