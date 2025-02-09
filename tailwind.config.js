25e7cc03145f3e147436ed071266d017fd167214
KN <ckn.kaien@gmail.com>
2025-02-09 12:52:20 +0800

    feat: add dark mode support and extend Tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: ["class", '[data-theme="darkTheme"]'],
   theme: {
     extend: {
       colors: {
         softWhite: "#fafafa",
+        steelBlue: "#393a4b",
+        formBackground: "#25273d",
+      },
+      screens: {
+        xs: "738px",
       },
     },
   },
