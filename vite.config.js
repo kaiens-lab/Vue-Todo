1983a1ac3e7e0c062b4b05409e772654c2b0f2ad
KN <ckn.kaien@gmail.com>
2025-02-11 17:51:43 +0800

    chore: update PostCSS configuration reference in Vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import postcss from "./postcss.config.js";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   css: {
-    postcss,
+    postcss: "./postcss.config.js",
   },
 });
