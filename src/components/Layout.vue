d38e9ee8c7e708eea54d56dc044f55b83f6c4488
KN <ckn.kaien@gmail.com>
2025-02-13 13:51:44 +0800

    chore(Layout): Move styles to auth.css file

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -7,6 +7,7 @@ import TodoHeader from "./TodoHeader.vue";
 import TodoList from "./TodoList.vue";
 import TodoFilter from "./TodoFilter.vue";
 import AppFooter from "./AppFooter.vue";
+import "../styles/auth.css";
 
 const todoStore = useTodoStore();
 
@@ -20,14 +21,14 @@ const themeStore = useThemeStore();
 
 <template>
   <div class="main__container">
-    <div class="todo">
+    <div class="w-full min-w-[300px] max-w-[540px] sm:min-w-0">
       <TodoHeader
         @addTodo="todoStore.addTodo"
         :isDarkMode="themeStore.isDarkMode"
         @toggleTheme="themeStore.toggleDarkMode"
       />
 
-      <main class="main">
+      <main>
         <TodoList
           @remove-todo="removeTodo"
           @toggle-todo="toggleTodoStatus"
@@ -47,80 +48,3 @@ const themeStore = useThemeStore();
     <router-view />
   </div>
 </template>
-
-<style scoped>
-[data-theme="darkTheme"] {
-  color: #c8cbe7;
-}
-
-.main__container {
-  width: 100vw;
-  min-height: 100vh;
-  background: url("/images/bg-desktop-light.jpg");
-  background-color: #fafafa;
-  background-repeat: no-repeat;
-  background-size: contain;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  position: relative;
-}
-
-[data-theme="darkTheme"] .main__container {
-  background: url("/images/bg-desktop-dark.jpg");
-  background-color: #171823;
-  background-repeat: no-repeat;
-  background-size: contain;
-}
-
-.btn {
-  cursor: pointer;
-  background-color: transparent;
-  border: none;
-  color: #9495a5;
-  font-size: 1rem;
-  line-height: 1rem;
-  font-family: inherit;
-}
-
-.todo {
-  width: 100%;
-  max-width: 540px;
-}
-
-.todo__circle,
-.todo__check {
-  width: 24px;
-  height: 24px;
-  border: 1px solid #e3e4f1;
-  border-radius: 50%;
-  background: transparent;
-}
-
-[data-theme="darkTheme"] .todo__circle,
-[data-theme="darkTheme"] .todo__check {
-  border: 1px solid #393a4b;
-}
-
-@media (max-width: 738px) {
-  .main__container {
-    padding: 0 15%;
-    background: url("/images/bg-mobile-light.jpg");
-    background-color: #fafafa;
-    background-repeat: no-repeat;
-    background-size: contain;
-  }
-
-  [data-theme="darkTheme"] .main__container {
-    background: url("/images/bg-mobile-dark.jpg");
-    background-color: #171823;
-    background-repeat: no-repeat;
-    background-size: contain;
-  }
-
-  .todo {
-    width: 100%;
-    min-width: 327px;
-  }
-}
-</style>
